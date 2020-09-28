6637e04d1802f7e58f26d63bbc9e7c303482863e
juniorlourenco <56567317+juniorlourenco@users.noreply.github.com>
2020-09-27 21:18:36 -0400

    get user by id

--- src/server.ts
+++ src/server.ts
@@ -2,17 +2,27 @@ import express from 'express';
 
 const app = express();
 
+const users = [
+    'Diego',
+    'Cleiton',
+    'Robson',
+    'Daniel',
+    'Junior'
+];
+
 app.get('/users', (request, response) => {
     console.log('Listagem de Usuários');
 
-    return response.json([
-        'Diego',
-        'Cleiton',
-        'Robson',
-        'Daniel',
-        'Junior'
-    ]);
+    return response.json(users);
 });
+
+app.get('/users/:id', (request, response) => {
+    const id = Number(request.params.id);
+
+    const user = users[id];
+
+    return response.json(user);
+})
 
 app.post('/users', (request, response) => {
     const user = {
